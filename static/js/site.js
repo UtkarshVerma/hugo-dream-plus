7000dd18a6333cd18aeeffbfce6f8922135faf6b
UtkarshVerma <utkarsh.naveen@gmail.com>
2018-04-10 09:36:27 +0530

    Cleaning and formatting code
    Former-commit-id: 0b1d2de5080492751e40883532979929f1610257

--- static/js/site.js
+++ static/js/site.js
@@ -1,75 +1,57 @@
 var SemanticUIColors = ["red", "orange", "yellow", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown"]
 
 $(document).ready(function () {
-  $('.dream-flip-toggle')
-    .click(function () {
-      var c = $('.pusher')
+  $('.dream-flip-toggle').click(function () {
+  	var target = $('.pusher')
 	  if (dreamBodyBgSwitchIndex === 0) {
-        if(colorBG)
-        	setBackground(dreamBody, dreamBodyBgSwitch[1])
-        dreamBodyBgSwitchIndex = 1
-        c.toggleClass('flip-it')
-        setTimeout(function(){
-            $('.back').css('display','block')    
-        	$('.front').css('display','none')
-        }, 200)
-      }
-      else {
-        if(colorBG)
-       		setBackground(dreamBody, dreamBodyBgSwitch[0])
-       	dreamBodyBgSwitchIndex = 0
-        c.toggleClass('flip-it')     
-        setTimeout(function(){
-            $('.front').css('display','block') 
-        	$('.back').css('display','none')
-        }, 200)      
-      }
-    })
-
-  var postList = $('.post-list')
-  var pMaxHeight = $(window).height() - $('.ui.menu').outerHeight(true)
-  postList.css('max-height', pMaxHeight)
-
-$(".dimmer").css("background-color","rgba(0,0,0,.6)");
-if(!(enabledPost)){
-	$('.ui.cards .image').dimmer({    	
-      	 on: 'hover'})
-  	 $('.ui.cards .image').dimmer('show') 
-}
-else{
-	if(enabledDimmer){		
-	$('.ui.cards .image').dimmer({    	
-      	 on: 'hover'})
-  	 $('.ui.cards .image').dimmer('show') 
-	}
-
-	else{
-	$('.ui.cards .image')
-	.dimmer({
-	 opacity: .6,
-         closable: false })
-	 $('.ui.cards .image').dimmer('show')
-	}
-}
-
-  $('.ui.accordion')
-    .accordion()
-
-  $('#tag-category-pop').click(function () {
-    var dt = $('.dream-tags')
-    var dc = $('.dream-categories')
-    var dtDisplay = dt.css('display')
-    var dcDisplay = dc.css('display')
-    if (dtDisplay === 'none') {
-      dt.css('display', 'block')
-      dc.css('display', 'block')
-    } else {
-      dt.css('display', 'none')
-      dc.css('display', 'none')
+    	if(colorBG)
+    	 	setBackground(dreamBody, dreamBodyBgSwitch[1])
+      dreamBodyBgSwitchIndex = 1
+      target.toggleClass('flip-it')
+    }
+    else {
+    	if(colorBG)
+    		setBackground(dreamBody, dreamBodyBgSwitch[0])
+     	dreamBodyBgSwitchIndex = 0
+      target.toggleClass('flip-it')        
     }
   })
+  
+	$('.dimmer').css('background-color','rgba(0,0,0,.6)')
+	if(!enabledPost) {
+		$('.ui.cards .image').dimmer({ on: 'hover'})
+ 		$('.ui.cards .image').dimmer('show') 
+	}
+	else {
+		if(enabledDimmer){		
+			$('.ui.cards .image').dimmer({ on: 'hover'})
+  		$('.ui.cards .image').dimmer('show') 
+		}
+		else {
+			$('.ui.cards .image')
+			.dimmer({ opacity: .6, closable: false })
+	 		$('.ui.cards .image').dimmer('show')
+		}
+	}
 
-  setSemanticUIColor()
+	$('.ui.accordion').accordion()
+
+	$('#tag-category-pop').click(function () {
+		var dt = $('.dream-tags')
+  	var dc = $('.dream-categories')
+  	var dtDisplay = dt.css('display')
+  	var dcDisplay = dc.css('display')
+  	if (dtDisplay === 'none') {
+    	dt.css('display', 'block')
+    	dc.css('display', 'block')
+  	}
+  	else {
+  		dt.css('display', 'none')
+    	dc.css('display', 'none')
+  	}
+	})
+	
+	setSemanticUIColor()
 })
 
 function randomInt(min, max) {
@@ -83,5 +65,5 @@ function setSemanticUIColor() {
   var tagsParent = $('.dream-tags')
   tagsParent.children().map(function() {
     $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)])
-  })
+  })  
 }
