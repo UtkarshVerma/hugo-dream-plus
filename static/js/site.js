87680f86e2537db81c7efced37a848d79129b847
UtkarshVerma <utkarsh.naveen@gmail.com>
2018-04-10 15:02:51 +0530

    Updated css and js
    Former-commit-id: 836995632a2cc7d91b9d66b3f906793ed570f66e [formerly b6bdccfdb784f05a68b05c0339e2129b347628b7]
    Former-commit-id: e0db5bce8472775e88198bc94413238c15fd1356
    Former-commit-id: 4c16b294f5418a64e28a4a80b4376aa5915b76be

--- static/js/site.js
+++ static/js/site.js
@@ -6,16 +6,25 @@ $(document).ready(function () {
 	  if (dreamBodyBgSwitchIndex === 0) {
     	if(colorBG)
     	 	setBackground(dreamBody, dreamBodyBgSwitch[1])
-      dreamBodyBgSwitchIndex = 1
-      target.toggleClass('flip-it')
-    }
+		dreamBodyBgSwitchIndex = 1
+		target.toggleClass('flip-it')
+		setTimeout( function () {
+			$('.front').css('display','none')
+			$('.back').css('display','block')
+		}, 300)
+
+	}
     else {
     	if(colorBG)
     		setBackground(dreamBody, dreamBodyBgSwitch[0])
-     	dreamBodyBgSwitchIndex = 0
-      target.toggleClass('flip-it')        
-    }
-  })
+     	dreamBodyBgSwitchIndex = 0     	
+     	target.toggleClass('flip-it')   
+     	setTimeout( function (){
+     		$('.front').css('display','block')
+				$('.back').css('display','none')
+			}, 300)			
+		}
+	})
   
 	$('.dimmer').css('background-color','rgba(0,0,0,.6)')
 	if(!enabledPost) {
@@ -54,6 +63,31 @@ $(document).ready(function () {
 	setSemanticUIColor()
 })
 
+function connect(arr) {
+	var str = ''
+	for (var i = 0; i < arr.length; i++) {
+		if (i !== arr.length - 1)
+			str += arr[i] + ', '
+		else
+			str += arr[i]
+	}
+	return str
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	var random
+	while (true) {
+		random = Math.floor(Math.random() * (max - min)) + min
+		if (random !== dreamPrevBgIndex) {
+			dreamPrevBgIndex = random
+     	break
+     }
+   }
+   return random
+}
+
 function randomInt(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
@@ -66,4 +100,9 @@ function setSemanticUIColor() {
   tagsParent.children().map(function() {
     $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)])
   })  
+}
+
+function setBackground(target, gradient) {
+	target.css({ background: gradient[0]})
+	target.css({ background: 'linear-gradient(to right, ' + connect(gradient) + ')'})
 }
