400fb695cfde359cd04aa421470e3f7b160e9b16
UtkarshVerma <utkarsh.naveen@gmail.com>
2018-04-19 07:31:15 +0530

    Optimizing loading
    Former-commit-id: 6b549a1a832fe64801538fc6e15e1ebb6c44efda [formerly 59a19b715252a0d737c1239b5941ac502eddbd32]
    Former-commit-id: 636c75bcbf07670bc0b3dc8fb6f5238113a57cf9
    Former-commit-id: c38346010d1f0a4ccd0e31f662c605e09b676abf

--- static/js/site.js
+++ static/js/site.js
@@ -1,65 +1,43 @@
-var SemanticUIColors = ["red", "orange", "yellow", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown"]
-
-$(document).ready(function () {
-	$('.ui.accordion').accordion()
-	
-	setSemanticUIColor()
-	
-	$('.youtube').height($('.youtube').width() * 0.6)
-	
-	$('#tag-category-pop').click(function() {
-		$('.link.hand.point.icon').toggleClass('active')
-	})
-})
-
-function connect(arr) {
-	var str = ''
-	for (var i = 0; i < arr.length; i++) {
-		if (i !== arr.length - 1)
-			str += arr[i] + ', '
-		else
-			str += arr[i]
-	}
-	return str
+var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
+$(document).ready(function() {
+  $(".ui.accordion").accordion();
+  setSemanticUIColor();
+  $(".youtube").height(0.6 * $(".youtube").width());
+  $("#tag-category-pop").click(function() {
+    $(".link.hand.point.icon").toggleClass("active");
+  });
+});
+function connect(a) {
+  for (var b = "", c = 0; c < a.length; c++) {
+    b = c !== a.length - 1 ? b + (a[c] + ", ") : b + a[c];
+  }
+  return b;
 }
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min)
-	max = Math.floor(max)
-	var random
-	while (true) {
-		random = Math.floor(Math.random() * (max - min)) + min
-		if (random !== dreamPrevBgIndex) {
-			dreamPrevBgIndex = random
-     	break
-     }
-   }
-   return random
+function getRandomInt(a, b) {
+  a = Math.ceil(a);
+  b = Math.floor(b);
+  for (var c;;) {
+    if (c = Math.floor(Math.random() * (b - a)) + a, c !== dreamPrevBgIndex) {
+      dreamPrevBgIndex = c;
+      break;
+    }
+  }
+  return c;
 }
-
-function randomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  random = Math.floor(Math.random() * (max - min)) + min
-  return random
+function randomInt(a, b) {
+  a = Math.ceil(a);
+  b = Math.floor(b);
+  return random = Math.floor(Math.random() * (b - a)) + a;
 }
-
 function setSemanticUIColor() {
-  var tagsParent = [$('.dream-tags'), $('.sidebar-dream-tags')]
-  for (var i = 0; i < tagsParent.length; i++)
-  	tagsParent[i].children().map(function() {
-		$(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)])
-	})
+  for (var a = [$(".dream-tags"), $(".sidebar-dream-tags")], b = 0; b < a.length; b++) {
+    a[b].children().map(function() {
+      $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
+    });
+  }
 }
-
-function setBackground(target, gradient) {
-	target.css({ background: gradient[0]})
-	target.css({ background: 'linear-gradient(to right, ' + connect(gradient) + ')'})
+function setBackground(a, b) {
+  a.css({background:b[0]});
+  a.css({background:"linear-gradient(to right, " + connect(b) + ")"});
 }
-
-function twemojify() {
-	console.log('Twemoji up and making stuff colourful!')
-	var classes = (document.getElementsByClassName('twemoji'))
-	for(var i = 0; i < classes.length; i++)
-		twemoji.parse(classes[i]);                
-}
+;
