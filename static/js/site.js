a3543892cf97fb36292e6cc0249b825a14bafd7d
UtkarshVerma <utkarsh.naveen@gmail.com>
2018-04-25 01:14:26 +0530

    Improve BG script and performance
    Former-commit-id: c9498c4f61d2c5bb5137384aba995c9b5621160a

--- static/js/site.js
+++ static/js/site.js
@@ -1,6 +1,4 @@
 var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
-var dreamPrevBgIndex = 0;
-var dreamBodyBgSwitchIndex = 0;
 
 $(document).ready(function() {
   $(".ui.accordion").accordion();
@@ -8,6 +6,26 @@ $(document).ready(function() {
   $("#tag-category-pop").click(function() {
     $(".hand.point.icon").toggleClass("active");
   });
+  
+  $('.dream-flip-toggle').click(function () {
+    var target = $('.pusher')
+    if (bodyBgSwitchIndex === 0) {
+      bodyBgSwitchIndex = 1
+      target.toggleClass('flip-it')
+      setTimeout( function () {
+        $('.front').css('display','none')
+        $('.back').css('display','block')
+      }, 300) 
+    }
+      else {
+        bodyBgSwitchIndex = 0      
+        target.toggleClass('flip-it')
+        setTimeout( function (){
+          $('.front').css('display','block')
+        $('.back').css('display','none')
+      }, 300)
+    }
+  })  
 });
 
 function randomInt(a, b) {
@@ -22,39 +40,4 @@ function setSemanticUIColor() {
       $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
     });
   }
-}
-
-function connect(arr) {
-  var str = ''
-  for (var i = 0; i < arr.length; i++) {
-    if (i !== arr.length - 1) {
-      str += arr[i] + ', '
-    } else {
-      str += arr[i]
-    }
-  }
-  return str
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  var random
-  while (1) {
-    random = Math.floor(Math.random() * (max - min)) + min
-    if (random !== dreamPrevBgIndex) {
-      dreamPrevBgIndex = random
-      break
-    }
-  }
-  return random
-}
-
-function setBackground(target, gradient) {
-  target.css({
-    background: gradient[0]
-  })
-  target.css({
-    background: 'linear-gradient(to right, ' + connect(gradient) + ')'
-  })
 }
