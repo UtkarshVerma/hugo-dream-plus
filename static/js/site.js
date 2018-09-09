ae6b2cd65ebdcee906408de8fcd086f220c77a9f
UtkarshVerma <utkarsh.naveen@gmail.com>
2018-09-09 15:54:18 +0530

    :art: Switch to staticly random colored tags
    Former-commit-id: 88d4139ab54c74425fbecbffb866c8be10ef133b [formerly bff2a040ebd2867199d0efcb4ff2259232c9d75e]
    Former-commit-id: 30b486be7f808229c0c4c2e01e4a2870f0617736
    Former-commit-id: 361836a6ca3f7d42d22b49a6fb88d5b0553a4e3a

--- static/js/site.js
+++ static/js/site.js
@@ -1,8 +1,5 @@
-var SemanticUIColors = "red orange yellow olive green teal blue violet purple pink brown".split(" ");
-
 $(document).ready(function() {
   $(".ui.accordion").accordion();
-  setSemanticUIColor();
   $("#tag-category-pop").click(function() {
     $(".hand.point.icon").toggleClass("active");
   });
@@ -27,17 +24,3 @@ $(document).ready(function() {
     }
   })
 });
-
-function randomInt(a, b) {
-  a = Math.ceil(a);
-  b = Math.floor(b);
-  return random = Math.floor(Math.random() * (b - a)) + a;
-}
-
-function setSemanticUIColor() {
-  for (var a = [$(".dream-tags"), $(".sidebar-dream-tags")], b = 0; b < a.length; b++) {
-    a[b].children().map(function() {
-      $(this).addClass(SemanticUIColors[randomInt(0, SemanticUIColors.length)]);
-    });
-  }
-}
